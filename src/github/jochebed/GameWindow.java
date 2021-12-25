9fb5ef026c91a11f9b927ecad418ed1b59590714
Snowiiii <Meieralexander65@gmail.com>
2021-12-25 16:16:16 +0100

    Init

--- src/github/jochebed/GameWindow.java
+++ src/github/jochebed/GameWindow.java
@@ -1,33 +1,26 @@
 package github.jochebed;
 
-import github.jochebed.graphics.TetrisPanel;
-
 import javax.swing.*;
 import java.awt.*;
 
-import static github.jochebed.ConstantsAndUtils.WINDOW_HEIGHT;
-import static github.jochebed.ConstantsAndUtils.WINDOW_WIDTH;
+import static github.jochebed.ConstantsAndUtils.*;
 
 public class GameWindow extends JFrame {
-  private final JPanel panel;
+    private final JPanel panel;
 
-  public GameWindow(String title, JPanel panel) {
-    super(title);
-    this.panel = panel;
-    this.load();
-  }
+    public GameWindow(String title, JPanel panel) {
+        super(title);
+        this.panel = panel;
+        this.load();
+    }
 
-  private void load() {
-    this.add(panel);
-    this.setSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
-    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    this.pack();
-    this.setLocationRelativeTo(null);
-    this.setVisible(true);
-    this.addKeyListener(panel.getKeyListeners()[0]);
-  }
-
-  public static void main(String[] args) {
-    new GameWindow("Tetris", new TetrisPanel());
-  }
+    private void load() {
+        this.add(panel);
+        this.setSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.addKeyListener(panel.getKeyListeners()[0]);
+    }
 }
